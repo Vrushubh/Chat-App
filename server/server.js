fa7f5cad805bca9a8d1c70e88aa8baf6cbae1cf8
Vrushubh <vrushubhgandhi@gmail.com>
2025-07-17 14:12:40 +0530

    Full server.js

--- server/server.js
+++ server/server.js
@@ -1,56 +1,59 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import http from 'http';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-import { Server } from 'socket.io';
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
-// Create Express app 
-const app = express();
-const server = http.createServer(app);
+let isConnected = false;
+let io;
+let server;
 
-// Initialize socket.io server
-export const io = new Server(server,{
-    cors : {origin: "*"}
-})
+const getServer = async () => {
+  const app = express();
 
-// Store online Users
-export const userSocketMap = {}; //{userId : socketId}
+  app.use(express.json({ limit: "4mb" }));
+  app.use(cors());
 
-// Socket.io Connection Handler
+  app.use("/api/status", (req, res) => res.send("Server is on"));
+  app.use("/api/auth", userRouter);
+  app.use("/api/messages", messageRouter);
 
-io.on("connection" , (socket)=>{
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected" , userId);
-    if(userId) userSocketMap[userId] = socket.id;
-    
-    //Emit online users to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  // Connect to MongoDB once
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
 
-    socket.on("disconnect" , ()=>{
-        console.log("User Disconnected" , userId);
+  server = http.createServer(app);
+
+  if (!io) {
+    io = new Server(server, {
+      cors: { origin: "*" },
+    });
+
+    const userSocketMap = {};
+
+    io.on("connection", (socket) => {
+      const userId = socket.handshake.query.userId;
+      console.log("User Connected", userId);
+      if (userId) userSocketMap[userId] = socket.id;
+
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+      socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers" , Object.keys(userSocketMap))
-        
-    })
-})
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+      });
+    });
+  }
 
-//Middleware setup
-app.use(express.json({limit : "4mb"}))
-app.use(cors());
+  return server;
+};
 
-// Routes Setup
-app.use("/api/status" , (req,res)=> res.send("Server is on"));
-app.use("/api/auth" , userRouter);
-app.use("/api/messages" , messageRouter);
-
-// Connect to MongoDB
-await connectDB();
-
-
-//Export sever for vercel
-export default (req, res) => {
-  return server.emit("request", req, res);
+export default async (req, res) => {
+  const srv = await getServer();
+  srv.emit("request", req, res);
 };
