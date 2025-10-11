d7cb68577319ddffa5fb1a68f63320df0261c87d
Vrushubh Gandhi <shantilalgandhi5782@gmail.com>
2025-10-11 19:05:34 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import {Toaster} from 'react-hot-toast'
 import { useContext } from 'react'
-import { AuthContext } from '../context/authContext'
+import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
   const { authUser } = useContext(AuthContext)
