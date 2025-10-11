21b53a02c0aab974a222d2532df9fde23e05e0ff
Vrushubh Gandhi <shantilalgandhi5782@gmail.com>
2025-10-11 19:41:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,10 @@ io.on("connection" , (socket)=>{
 
 //Middleware setup
 app.use(express.json({limit : "4mb"}))
-app.use(cors());
+app.use(cors({
+  origin: ["https://chat-app-ryd8.vercel.app"], // your Vercel URL
+  credentials: true
+}));
 
 // Routes Setup
 app.use("/api/status" , (req,res)=> res.send("Server is on"));
